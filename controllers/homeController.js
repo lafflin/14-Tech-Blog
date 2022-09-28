1299428f0baca79cc7d7cd97a6dbb104815dcad1
Max McLaughlin <maxwelljmack@gmail.com>
2022-09-27 21:27:12 -0700

    got the home page to show the inital posts now

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,13 +1,44 @@
 const router = require("express").Router();
 // const apiController = require("./apiController");
-const { User } = require("../models");
+const sequelize = require("../config/connection");
+const { Comment, Post, User } = require("../models");
 
 router.get("/", (req, res) => {
-	res.render("landingPage", {
-		isLoggedIn: req.session.user,
+	Post.findAll({
+		attributes: ["id", "title", "content", "createdAt"],
+		include: [
+			{
+				model: Comment,
+				attributes: [
+					"id",
+					"comment_content",
+					"user_id",
+					"post_id",
+					"createdAt",
+				],
+				include: {
+					model: User,
+					attributes: ["username"],
+				},
+			},
+			{
+				model: User,
+				attributes: ["username"],
+			},
+		],
+	}).then((postData) => {
+		const posts = postData.map((post) =>
+			post.get({
+				plain: true,
+			})
+		);
+
+		res.render("landingPage", {
+			posts,
+			isLoggedIn: req.session.user,
+		});
 	});
 });
-
 // router.use("/api", apiController);
 
 module.exports = router;
